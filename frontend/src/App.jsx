24827c55f552fcd429d7a706c9bb804e45aeab8c
Aradhyay <om622dubey@gmail.com>
2025-02-22 13:18:34 +0530

    Initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,25 @@
-import { useState } from "react";
+// import { useState } from "react";
 import Navbar from "./components/Navbar";
 import { Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
-import LoginPage from "./components/LoginPage/LoginPage"; 
+// import LoginPage from "./components/LoginPage/LoginPage"; 
 
 const App = () => {
-  const [showLogin, setShowLogin] = useState(false);
+  // const [showLogin, setShowLogin] = useState(false);
 
   return (
-    <>
-      {showLogin?<LoginPage setShowLogin={setShowLogin} />:<></>}
+  
+     
       <div className="app">
-        <Navbar setShowLogin={setShowLogin} />
+        <Navbar  />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/order' element={<PlaceOrder />} />
         </Routes>
       </div>
-    </>
   );
 }
 
